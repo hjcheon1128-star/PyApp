548f41785a98f5ad0662c0d04c74948559240985
hjcheon1128-star <hj.cheon.1128@gmail.com>
2026-02-07 11:34:34 +0000

    코드변경1

--- streamlit_main.py
+++ streamlit_main.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
-st.title("Hello Streamlit")
+st.title("안녕하세요 Streamlit")
+
+st.header("헤더를 입력할 수 있어요")
+
+st.subheader("이것은 subheader입니다")
+
+st.caption("캡션을 넣어봅니다")
+
+sample_code = '''
+def function():
+    print('hello, world')
+    '''
+st.code(sample_code)
